480ac8195d4eb974ccc353a93e77aecb7de96353
Adam Newhouser <adamnewhouser@gmail.com>
2020-02-08 15:08:08 -0500

    commit

---
+++ build/precache-manifest.c26b21b949c49a3dd741a2de720148ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985d117afac9084420de96e8481cdc27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13bf55b17e5d0b3248a4",
+    "url": "/static/css/main.0f332fc3.chunk.css"
+  },
+  {
+    "revision": "5d4b5d4b28fcfe5c0681",
+    "url": "/static/js/2.2adbaff8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2adbaff8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13bf55b17e5d0b3248a4",
+    "url": "/static/js/main.a3cfec6b.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
